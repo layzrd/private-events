391f3adf9286591d41098f8120ad75d3caaaf703
Elias Castañeda <e71az@icloud.com>
2020-09-01 09:34:49 -0600

    add: destroy session method to session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,13 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    User.where(id: session[:user_id]).first
+    User.where(id: session[:user_id]).first if session[:user_id]
   end
 
   def signed_in?
     return true if current_user&.id
 
     false
-  end
-
-  def destroy
   end
 
   helper_method :current_user, :signed_in?
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,19 +1,18 @@
 class SessionController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(username: params[:username])
     if @user
-      session[:user_id] = @user.id 
+      session[:user_id] = @user.id
       redirect_to users_path
     else
-      render :new 
+      render :new
     end
-    
-
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to session_new_path
   end
 end
