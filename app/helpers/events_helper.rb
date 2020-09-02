15d1dc07cffcbc7b8625abce623fb938c2d2de9b
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-09-02 22:56:19 +0300

    add list_events helper and refactor views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,10 @@
 module EventsHelper
+  def list_events(obj)
+    content_tag(:ul, :class => 'bg-gray-200') do
+      obj.each do |event|
+        concat content_tag(:li, "#{event.description} on #{event.updated_at.to_s(:short)}")
+        concat link_to 'Show', event
+      end
+    end
+  end
 end
