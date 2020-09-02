4dcb5442f44ee0e83c576ea968c3cf5068062e13
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-09-02 15:29:32 +0300

    add conditional flash message

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,12 +2,18 @@ class SessionController < ApplicationController
   def new; end
 
   def create
+    if params.has_key?(:username).present?
+      flash[:alert] = 'Please provide username.'
+      redirect_to session_new_path
+      return
+    end
     @user = User.find_by(username: params[:username])
     if @user
       session[:user_id] = @user.id
       redirect_to users_path
     else
-      render :new
+      flash[:alert] = 'the given username is not exist.'
+      redirect_to session_new_path
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :username, presence: true
+
   scope :select_specific_fields, -> { pluck('users.name, events.location, events.description, events.event_date') }
   scope :join_event, -> { joins(user_events: :event) }
+
   has_many :my_events, class_name: 'Event'
   has_many :user_events
   has_many :attended_event, through: :user_events, source: :event
-  validates :name, presence: true
-  validates :username, presence: true
 
   def self.upcoming_events
     join_event.where('event_date >= ?', Date.today.to_s(:db)).select_specific_fields
