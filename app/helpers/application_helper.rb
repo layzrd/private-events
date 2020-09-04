3409115e728f2c7b2178755f5db8e7c289b2a558
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-09-04 19:19:16 +0300

    fix rubocop linting errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,64 +1,67 @@
 module ApplicationHelper
-    def conditional_view(data, cls: '')
-        if data.is_a?(Array)
-            data.each do |item|
-                concat content_tag(:p, item, class: cls)
-            end
-        else
-            content_tag(:p, data, class: cls) if data
+  def conditional_view(data, cls: '')
+    if data.is_a?(Array)
+      data.each do |item|
+        concat content_tag(:p, item, class: cls)
+      end
+    else
+      content_tag(:p, data, class: cls) if data
+    end
+  end
+
+  def signed_in?
+    return true if session[:user]
+
+    false
+  end
+
+  def authenticated?
+    return if signed_in?
+
+    flash[:error] = 'Please login first'
+    redirect_to new_session_path
+  end
+
+  def conditional_link(cond, link)
+    if cond
+      if link.is_a?(Array)
+        link.each do |l|
+          concat link_to l[:lbl], l[:path], method: l[:mtd]
         end
+      else
+        link_to link[:lbl], link[:path]
+      end
     end
-    
-    def signed_in?
-        return true if session[:user]
-    
-        false
-    end
-    
-    def authenticated?
-        return if signed_in?
-    
-        flash[:error] = 'Please login first'
-        redirect_to new_session_path
-    end
+  end
 
-    def conditional_link(cond, link)
-        if cond
-            if link.is_a?(Array)
-                link.each do |l|
-                    concat link_to l[:lbl], l[:path], method: l[:mtd]
-                end
-            else
-                link_to link[:lbl], link[:path]
-            end
-        end
+  def authorized_links!
+    if signed_in?
+      concat link_to 'events', events_path
+      concat link_to 'users', users_path
+      link_to 'log out', session_path(current_user), method: :delete
+    else
+      concat link_to 'sign up', new_user_path
+      link_to 'log in', new_session_path
     end
+  end
 
-    def authorized_links!
-        if signed_in?
-            concat link_to 'events', events_path
-            concat link_to 'users', users_path
-            link_to 'log out', session_path(current_user), method: :delete
-        else
-            concat link_to 'sign up', new_user_path
-            link_to 'log in', new_session_path
-        end
-    end
+  def current_user
+    return unless session[:user_id]
 
-    def current_user
-        return unless session[:user_id]
-        @current_user ||= User.find_by_id(session[:user_id])
-    end
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 
-    def current_user_name
-        current_user.name if signed_in?
-    end
+  def current_user_name
+    current_user.name if signed_in?
+  end
 
-    def error_view errors
-        if errors.any?  
-            concat content_tag(:div,
-                content_tag(:h2, "#{pluralize(errors.count, "error")} prohibited this user from being saved:", class: ""))
-            conditional_view(errors.full_messages)
-        end
+  def error_view errors
+    if errors.any?
+      concat content_tag(:div,
+                         content_tag(:h2,
+                                     "#{pluralize(errors.count, "error")}
+                                    prohibited this user from being saved:", class: ""))
+      conditional_view(errors.full_messages)
     end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
-    def list_user(obj)
-        content_tag(:ul, class: 'bg-gray-200') do
-          obj.each do |user|
-            concat content_tag(:li, "#{user.name} (@#{user.username})")
-            concat link_to 'Show', user
-          end
-        end
+  def list_user(obj)
+    content_tag(:ul, class: 'bg-gray-200') do
+      obj.each do |user|
+        concat content_tag(:li, "#{user.name} (@#{user.username})")
+        concat link_to 'Show', user
       end
+    end
+  end
 end
